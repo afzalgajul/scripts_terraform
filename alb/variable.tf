13896aae1cb85f561008e4cd5513ebf04acc14db
afzalgajul <afzal.shaik@mahiratechnology.com>
2023-02-27 21:50:51 +0530

    added alb

---
+++ alb/variable.tf
@@ -0,0 +1,11 @@
+variable "security_groups" {
+  type    = string
+  default = "sg-02d4d2ed96dbef4b9"
+}
+
+variable "subnet1" {
+  default = "subnet-0d703f887c0340385"
+}
+variable "subnet2" {
+  default = "subnet-03846a56f7c1381a6"
+}
